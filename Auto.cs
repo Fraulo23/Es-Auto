747c9960de562c7b07f1915a11a1a71395d2e7a2
Fraulo23 <fraulinifederico@gmail.com>
2024-01-17 16:48:53 +0100

    Aggiungere i file di progetto.

---
+++ Auto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_Auto
+{
+    internal class Auto
+    {
+        public string Modello { get; set; }
+        public Auto(string modello)
+        {
+            Modello = modello;
+        }   
+        public void CambioModello(string modello)
+        {
+            Modello = modello;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_Auto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto a1 = new Auto("Fiat");
+            Auto a2 = a1;
+            a2.CambioModello("Mercedes");
+            Console.WriteLine(a1.Modello);
+            Console.WriteLine(a2.Modello);
+            Auto a3 = new Auto("Lancia");
+            a1 = a3;
+            Console.WriteLine(a1.Modello);
+            Console.WriteLine(a3.Modello);
+
+            Console.ReadLine();
+        }
+    }
+}
